2362fa9bb1ce05e8b1a5bb9bec7ffd0dc4be1620
Klaus Dimde <klaus@dimde.info>
2024-06-11 18:08:57 +0200

    Reimplemented File

--- Lib/File.php
+++ Lib/File.php
@@ -4,30 +4,86 @@ namespace Sunhill\Dedup;
 
 class File
 {
+
+    protected $file = '';
     
-    protected $file;
+    protected $file_size = 0;
     
-    public function __construct(string $file)
+    protected $short_hash = '';
+    
+    protected $long_hash = null;
+    
+    protected $mime_group = '';
+    
+    protected $mime_subgroup = '';
+    
+    protected $state;
+
+    protected $id = 0;
+    
+    public function readFile(string $path)
     {
-        $this->file = $file;    
+        $this->file = $path;
+        $this->file_size = filesize($path);
+        list($this->mime_group,$this->mime_subgroup) = explode('/', mime_content_type($this->file));
+    }
+    
+    public function filePath(): string
+    {
+        return $this->file;
     }
     
     public function longHash(): string
     {
-        return sha1_file($this->file);
+        if (empty($this->long_hash)) {
+            $this->long_hash = sha1_file($this->file);
+        }
+        return $this->long_hash;
     }
     
     public function shortHash(): string
     {
-        $fp = fopen($this->file, 'r');
-        $data = fread($fp, 40);
-        fclose($fp);
-        return sha1($data);
+        if (empty($this->short_hash)) {
+            $fp = fopen($this->file, 'r');
+            $data = fread($fp, 40);
+            $this->short_hash = sha1($data);
+            fclose($fp);            
+        }
+        return $this->short_hash;
     }
     
     public function size(): int
     {
         return filesize($this->file);
     }
+
+    public function setState(string $state)
+    {
+        $this->state = $state;
+    }
     
+    public function getState(): string
+    {
+        return $this->state;
+    }
+    
+    public function getMimeGroup(): string
+    {
+        return $this->mime_group;
+    }
+    
+    public function getMimeSubgroup(): string
+    {
+        return $this->mime_subgroup;
+    }
+    
+    public function setID(int $id)
+    {
+        $this->id = $id;
+    }
+    
+    public function getID(): int
+    {
+        return $this->id;
+    }
 }
--- tests/Unit/FileTest.php
+++ tests/Unit/FileTest.php
@@ -4,18 +4,47 @@ use Sunhill\Dedup\File;
 
 test('Long hash works', function()
 {
-    $test = new File(dirname(__FILE__).'/Samples/testfile.txt');
+    $test = new File();
+    $test->readFile(dirname(__FILE__).'/Samples/testfile.txt');
     expect($test->longHash())->toBe('c5cf529d4635f32dc94969faba60fd333184c267');
 });
 
 test('Short hash works', function()
 {
-    $test = new File(dirname(__FILE__).'/Samples/testfile.txt');
+    $test = new File();
+    $test->readFile(dirname(__FILE__).'/Samples/testfile.txt');
     expect($test->shortHash())->toBe('e94e5d9950014c27804be1df96ca8ad420b90a89');    
 });
 
 test('File size works', function()
 {
-    $test = new File(dirname(__FILE__).'/Samples/testfile.txt');
+    $test = new File();
+    $test->readFile(dirname(__FILE__).'/Samples/testfile.txt');
     expect($test->size())->toBe(80);    
 });
+
+test('File mime group works', function($file, $expect)
+{
+    $test = new File();
+    $test->readFile(dirname(__FILE__).'/../SampleFiles/testfiles/'.$file);
+    expect($test->getMimeGroup())->toBe($expect);    
+})->with([
+    ['audio-flac/test.flac','audio'],   
+    ['audio-mp3/test.mp3','audio'],
+    ['image-heif/test.heic','image'],
+    ['image-jpeg/test.jpg','image'],
+    ['application-testfile/A.qqq','text']
+]);
+
+test('File mime subgroup works', function($file, $expect)
+{
+    $test = new File();
+    $test->readFile(dirname(__FILE__).'/../SampleFiles/testfiles/'.$file);
+    expect($test->getMimeSubGroup())->toBe($expect);
+})->with([
+    ['audio-flac/test.flac','flac'],
+    ['audio-mp3/test.mp3','mpeg'],
+    ['image-heif/test.heic','heif'],
+    ['image-jpeg/test.jpg','jpeg'],
+    ['application-testfile/A.qqq','plain']
+]);
